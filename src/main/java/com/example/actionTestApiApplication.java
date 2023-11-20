a4474b29c67d229fff36597d60d689ae78112aa9
Zhanglei <133573238+Chou3ishi@users.noreply.github.com>
2023-11-20 16:36:17 +0900

    Update actionTestApiApplication.java

--- src/main/java/com/example/actionTestApiApplication.java
+++ src/main/java/com/example/actionTestApiApplication.java
@@ -1,18 +1,14 @@
-package com.kantsu.api;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-/**
- * @author Taku Kyo
- * @version 1.0
- */
 @SpringBootApplication
 @EnableScheduling
 public class KantsuApiApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(KantsuApiApplication.class, args);
+        System.out.pringln("hello test Action!!!");
+        //SpringApplication.run(KantsuApiApplication.class, args);
     }
 }
